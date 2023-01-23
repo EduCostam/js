8fd55b185f79fa82acb4d9c6a9911415b9f11b2a
EduCostam <eduardomc.devs@gmail.com>
2023-01-23 15:28:16 -0300

    particularidades-js

---
+++ particularidades_JS/scripts.js
@@ -0,0 +1,21 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+// linguagem fracamente tipada
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+/*
+  Exemplo
+  de 
+  comentário
+  de 
+  várias
+  linhas
+*/
