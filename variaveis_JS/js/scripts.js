3ddf8fab851925769975862017eca7145f80e318
EduCostam <eduardomc.devs@gmail.com>
2023-01-23 16:22:44 -0300

    let var const & camelCase

---
+++ variaveis_JS/js/scripts.js
@@ -0,0 +1,43 @@
+var teste = 1;
+
+nome = "João"
+
+console.log(teste);
+
+teste = 'Eduardo';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Eduardo';
+
+console.log(meuPrimeiroNome); // camelCase
+
+var meusobrenome = 'Marques Costa';
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'Eduardo';
+
+console.log(meuNome);
