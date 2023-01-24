f5d71595d766bca14b987c0af2397bfdf63c02f1
EduCostam <eduardomc.devs@gmail.com>
2023-01-24 03:09:47 -0300

    var ap

---
+++ geral_JS/js/script.js
@@ -0,0 +1,14 @@
+// let idade = 10;
+
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// camelCase;
+
+let nome = 'Eduardo';
+let idade = 25;
+let altura = 1.75;
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
