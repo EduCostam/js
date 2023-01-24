6e4b6ad82490fb93f80d439346d8f6330b85d65f
EduCostam <eduardomc.devs@gmail.com>
2023-01-24 03:25:31 -0300

    doc.write var concatena

---
+++ string_JS/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Eduardo";
+var sobrenome = "Marques Costa"
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.2556468");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse: "Olá"');
+
+//Concatenação
+console.log("Este número: " + "432");
