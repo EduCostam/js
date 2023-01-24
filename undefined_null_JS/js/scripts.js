544ee75a2d9b9ebfe40ca19466a7010a8ea92eec
EduCostam <eduardomc.devs@gmail.com>
2023-01-24 14:22:04 -0300

    hosting - içamento

---
+++ undefined_null_JS/js/scripts.js
@@ -0,0 +1,14 @@
+// Hosting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = "Marques Costa";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Eduardo";
+console.log(nome);
+
+var numero = 5;
