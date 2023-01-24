b3afa076fe3f1021dfa885213918e18fce2a08a0
EduCostam <eduardomc.devs@gmail.com>
2023-01-24 16:04:14 -0300

    arreays

---
+++ geral_JS/arrays/scripts.js
@@ -0,0 +1,8 @@
+// Arrays 
+
+let familia = [true,53,35,'Eduardo',40,69];
+console.log(familia.length);
+console.log(familia[0]);
+let nomeDoColega = ['Artur','Xanxere',23];
+console.log(nomeDoColega[0]);
+console.log(nomeDoColega.length);
