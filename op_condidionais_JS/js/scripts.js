31c9ad83c12c1ccecf05c7c42bc1084bf543394a
EduCostam <eduardomc.devs@gmail.com>
2023-01-26 16:28:32 -0300

    if

---
+++ op_condidionais_JS/js/scripts.js
@@ -0,0 +1,15 @@
+var idade = 16;
+var idadeMinima = 18;
+
+
+console.log("Antes do if");
+
+if(idade > 18) {
+    console.log("pode fazer a carteira de habilitação");
+}
+
+if(idade > 15) {
+    console.log("Precisa esperar 3 anos ainda");
+}
+
+console.log("Depois do if");
