c46a5b91e9f0d1733ee280dc9a105ae55751ed6d
EduCostam <eduardomc.devs@gmail.com>
2023-01-28 20:54:36 -0300

    if else

---
+++ op_comparação_JS/js/scripts.js
@@ -0,0 +1,30 @@
+var idade = 17;
+var possuiCarro = 1;
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer a carteira");
+} else {
+    console.log("O usuário não pode fazer acarteira");
+} 
+
+if(possuiCarro) {
+    console.log("O usuário ja pode andar de carro");
+}
+
+var nome = "Eduardo";
+
+if(nome == "Eduardo") {
+    console.log("O seu nome é Eduardo");
+}
+
+if(nome != "Eduardo") {
+    console.log("O nome não é Eduardo");
+}
+
+if(20 > 100) {
+    console.log("Passou");
+}
+
+if(100 < 20) {
+    console.log("Passou 2");
+}
