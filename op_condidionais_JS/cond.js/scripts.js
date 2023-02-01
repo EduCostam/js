71d50cc33b09d2b1615259f1e63f6d66eb90724b
EduCostam <eduardomc.devs@gmail.com>
2023-02-01 17:48:36 -0300

    else if

---
+++ op_condidionais_JS/cond.js/scripts.js
@@ -0,0 +1,23 @@
+var nome = 'Mateus';
+
+if(nome == 'Eduardo') {
+    console.log("O nome dele é Eduardo");
+} else if (nome == "Mateus") {
+    console.log("O  nome é Mateus");
+} else {
+    console.log("Ele possui outro  nome!");
+}
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+} else if (idade  >= 18) {
+    console.log("Ele só pode entrar com autorização");
+}
+
+if(nome = "Eduardo") {
+    console.log("teste");
+} else {
+    console.log("testando");
+}
