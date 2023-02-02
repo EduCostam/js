b4bc172de70356e4154041d147ca1a8a3495adea
EduCostam <eduardomc.devs@gmail.com>
2023-02-02 16:08:54 -0300

    || and !

--- op_logicos.JS/js/scripts.js
+++ op_logicos.JS/js/scripts.js
@@ -14,5 +14,32 @@ if(1 == 1 && 3 > 2 && true) {
 if((1 == 1 && 3 > 3) && true) {
     console.log("Passou");
 } else if(nome === "João" && idade >= 14) {
-    console.log("Aqui passa!");
+    console.log("Aqui passa!"); 
+}
+
+// Operador OR ||
+var idade = 16;
+var nome = "João";
+
+if(nome == "João" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima");
+} else {
+    console.log("Não pode entrar");
+}
+
+if(nome == "João" && 15 > 20  || 10 == 10) {
+    console.log("testando");
+} else {
+    console.log("não entrou");
+}
+
+// Operador !
+if(!false) {
+    console.log("Passou");
+}
+
+var nome = "Eduardo";
+
+if(!(nome == "Eduardo")) {
+    console.log("Ok");
 }
