6ad1fff17618a426b8af86297de918ecf78d9cd3
EduCostam <eduardomc.devs@gmail.com>
2023-02-02 16:26:43 -0300

    while

---
+++ estruturas_repeticao_JS/js/scripts.js
@@ -0,0 +1,29 @@
+var x = 0;
+
+while(x < 5) {
+
+    console.log("Testanto repetição " + x);
+
+    // incrementador
+    x++;
+
+}
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+    console.log(arr[y]);
+
+    y++;
+}
+
+var palavra = "Eduardo";
+var i = 0;
+
+while(i <= 7) {
+
+    console.log(palavra[i]);
+
+    i += 1;
+}
