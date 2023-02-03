69074a33502c0b37500d64777e10fd5719f00b99
EduCostam <eduardomc.devs@gmail.com>
2023-02-02 22:49:04 -0300

    for

--- estruturas_repeticao_JS/js/scripts.js
+++ estruturas_repeticao_JS/js/scripts.js
@@ -27,3 +27,21 @@ while(i <= 7) {
 
     i += 1;
 }
+
+for(var i = 0; i < 10; i++) {
+
+    console.log("Repetindo for: " + i);
+
+}
+
+var arr = [1,2,3,4];
+
+for(var j =0; j < arr.length; j++) {
+    console.log(arr[j]);
+}
+
+console.log(arr.length);
+
+for(var x = 5; x < 100; x *= 3) {
+    console.log(x);
+}
