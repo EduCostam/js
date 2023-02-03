54ca604721c0241a2b214fc0ba5e925dbaacb440
EduCostam <eduardomc.devs@gmail.com>
2023-02-03 13:28:16 -0300

    blocos de código reutilizáveis

---
+++ funcoes_JS/js/scripts.js
@@ -0,0 +1,37 @@
+function primeiraFuncao() {
+
+
+    console.log("Hello word das funções");
+
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+
+    console.log("O nome é: " + nome);
+
+}
+
+dizerNome("Eduardo");
+dizerNome("Augusto");
+dizerNome("Brocolis");
+
+var nomeDoBancoDeDados = "João";
+
+dizerNome(nomeDoBancoDeDados);
+
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+
+console.log(somaDois);
