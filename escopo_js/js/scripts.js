b7c6d19e2eaa745222663d88a3ee4d2d7e7dbebd
EduCostam <eduardomc.devs@gmail.com>
2023-02-06 16:33:03 -0300

    let + const + escopo

--- escopo_js/js/scripts.js
+++ escopo_js/js/scripts.js
@@ -1,3 +1,4 @@
+/*
 var x = 1;
 
 var y = 3;
@@ -32,4 +33,39 @@ if(true) {
 
 }
 
-console.log(p);
+console.log(p); */
+
+// let e const + escopo
+let x = 5; // var x = 5;
+
+const y = 10;
+
+x = 12;
+
+console.log(x);
+
+if(true) {
+
+    let x = 20;
+
+    console.log(x);
+
+    const y = 50;
+
+    console.log('const if ' + y);
+
+}
+
+console.log(x);
+
+if(20 > 10) {
+
+    const y = 100;
+
+    console.log('const if 3 '+ y);
+
+}
+
+for(let x = 0; x < 10; x++) {
+    console.log(x);
+}
