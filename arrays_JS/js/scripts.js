5d23d340524616912b7ea68f5d4f7ef8fc09f2de
EduCostam <eduardomc.devs@gmail.com>
2023-02-09 18:34:30 -0300

    Funções array

--- arrays_JS/js/scripts.js
+++ arrays_JS/js/scripts.js
@@ -18,3 +18,61 @@ arr[0] = "Teste";
 console.log(arr);
 
 console.log(typeof arr);
+
+// lengt
+var arr = [1,2,3,4,5];
+
+console.log(arr.length);
+
+// push
+arr.push(6);
+arr.push('Qualquer coisa');
+
+console.log(arr);
+
+// pop
+arr.pop();
+
+console.log(arr);
+
+// unshift
+arr.unshift(0);
+arr.unshift('teste');
+
+console.log(arr);
+
+// shift
+arr.shift();
+
+console.log(arr);
+
+// acessar o último elemento
+console.log(arr[arr.length -1]);
+
+// isArray
+console.log(Array.isArray(5));
+
+console.log(Array.isArray(arr));
+
+// splice
+var arr = [1,2,3,4,5];
+
+arr.splice(2, 0, 999);
+
+console.log(arr);
+
+arr.splice(4, 1);
+
+console.log(arr);
+
+// indexof
+console.log(arr.indexOf(5));
+
+
+// join
+var arr2 = ["O", "rato", "roeu", "a", "roupa"];
+
+console.log(arr2.join(","));
+
+// reverse
+console.log(arr2.reverse());
