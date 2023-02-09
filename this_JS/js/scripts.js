da3d448513a8d90da126a698a10cbcc7a167c1a2
EduCostam <eduardomc.devs@gmail.com>
2023-02-09 20:24:25 -0300

    this

---
+++ this_JS/js/scripts.js
@@ -0,0 +1,35 @@
+var teste = 5;
+
+console.log(this);
+console.log(this.teste);
+console.log(teste);
+
+
+let pessoa = {
+    nome: "Eduardo",
+    idade: 25,
+    falar: function() {
+        console.log("Olá, tudo bem?")
+    },
+    dizerNome: function() {
+        console.log("O meu nome é " + this.nome);
+    },
+    aniversario: function() {
+        this.idade += 1;
+    },
+    saudacao: function() {
+        return 'Sr. ' + this.nome;
+    }
+};
+
+pessoa.dizerNome();
+
+console.log(pessoa.idade);
+
+pessoa.aniversario();
+pessoa.aniversario();
+pessoa.aniversario();
+
+console.log(pessoa.idade);
+
+console.log(pessoa.saudacao());
