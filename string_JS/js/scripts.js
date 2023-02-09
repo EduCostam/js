2f436fa2cdf37d9c413dfd8f51492b9ba451f4a4
EduCostam <eduardomc.devs@gmail.com>
2023-02-09 17:31:11 -0300

    Funções e métodos de String

--- string_JS/js/scripts.js
+++ string_JS/js/scripts.js
@@ -23,3 +23,64 @@ document.write('Ele disse: "Olá"');
 
 //Concatenação
 console.log("Este número: " + "432");
+
+// lengt
+var nome = "Eduardo";
+
+console.log(nome.length);
+
+var obj = "bola";
+
+console.log(obj.length);
+
+// indexOf
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice
+
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+// replace
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
+
+// toLowerCase e toUpperCase
+
+var frase = "Esta é a frase que vamos manipular";
+
+var fraseCaixaAlata = frase.toUpperCase();
+
+console.log(fraseCaixaAlata);
+
+console.log(frase.toLocaleLowerCase());
+
+// trim
+
+var nome = "   Eduardo   ";
+
+var nomeTrim = nome.trim();
+
+console.log(nome);
+console.log(nomeTrim);
+
+// split
+
+console.log(frase.split(" "));
+
+var tags = "PHP, JavaScript, HTML, CSS";
+
+console.log(tags.split(", "));
+
+// lastIndexOf
+
+var fraseDois = "Eu quero a última palavra teste desta frase de testes";
+
+console.log(fraseDois.indexOf("teste"));
