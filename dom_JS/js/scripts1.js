668e0f14aa94d7530c024d541e173dd5158728c0
EduCostam <eduardomc.devs@gmail.com>
2023-02-16 17:06:41 -0300

    dom

---
+++ dom_JS/js/scripts1.js
@@ -0,0 +1,11 @@
+// tag
+var titulo = document.getElementsByTagName("h1")[0];
+console.log(titulo);
+
+// id
+var paragrafo = document.getElementsById("paragrafo");
+console.log(paragrafo);
+
+// class
+var itensDaLista = document.getElementsByClassName("item");
+console.log(itensDaLista);
