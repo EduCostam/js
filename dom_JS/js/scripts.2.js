5e67a904d0c3335df440efb0f2e661c0dda54468
EduCostam <eduardomc.devs@gmail.com>
2023-02-18 20:33:51 -0300

    querySelectorAll querySelector

---
+++ dom_JS/js/scripts.2.js
@@ -0,0 +1,21 @@
+var intemClasse = document.getElementsByClassName(".item");
+
+console.log(intemClasse);
+
+// querySelectorAll
+var itemQuery = document.querySelectorAll("#lista li");
+
+console.log(itemQuery);
+
+var itemQuery2 = document.querySelectorAll("#lista .item");
+
+console.log(itemQuery2);
+
+// querySelector
+var lista = document.querySelectorAll("#lista");
+
+console.log(lista);
+
+var primeiraLista = document.querySelectorAll("ul");
+
+console.log(primeiraLista);
